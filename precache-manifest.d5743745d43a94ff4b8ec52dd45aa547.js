800384d1aec6ec2dde21dea93ddcbce40796f0e7
Aleksandr Savkin <voyager5874@gmail.com>
2022-01-15 12:53:36 +0300

    Updates

---
+++ precache-manifest.d5743745d43a94ff4b8ec52dd45aa547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe90ccb7945fa28e3328ecef8a8bb86e",
+    "url": "/it-inc-todolist-ts/index.html"
+  },
+  {
+    "revision": "3c5c051246af2379975a",
+    "url": "/it-inc-todolist-ts/static/css/main.e5b21dbf.chunk.css"
+  },
+  {
+    "revision": "66674042fa131b100717",
+    "url": "/it-inc-todolist-ts/static/js/2.8a9aa02f.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/it-inc-todolist-ts/static/js/2.8a9aa02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5c051246af2379975a",
+    "url": "/it-inc-todolist-ts/static/js/main.7e20b91a.chunk.js"
+  },
+  {
+    "revision": "50abc6d0122b09419380",
+    "url": "/it-inc-todolist-ts/static/js/runtime-main.8fa8f134.js"
+  }
+]);
