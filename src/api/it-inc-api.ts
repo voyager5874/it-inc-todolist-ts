d98ec6120e2b476ca57e9e33b63f175e0fe4245b
Aleksandr Savkin <voyager5874@gmail.com>
2022-02-05 15:09:22 +0300

    create a mini app to access the server within stories

--- src/api/it-inc-api.ts
+++ src/api/it-inc-api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
-import {EINPROGRESS} from "constants";
+import {itIncAPI} from "./apiKey";
 
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': 'dd744663-c08a-476e-8a84-c072bf5b7a68'
+        'API-KEY': itIncAPI
     }
 })
 
@@ -43,33 +43,65 @@ export type TaskType = {
     todoListId: string
     order: number
     addedDate: string
-
 }
 
+type BaseResponseType<T = {}> = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors: Array<string>
+    data: T
+}
+
+type GetTasksResponseType = {
+    error: null | string
+    items: Array<TaskType>
+    totalCount: number
+}
+
+type UpdateTaskRequestBodyType = {
+    title: string
+    description: string
+    completed: boolean
+    status: number
+    priority: number
+    startDate: string
+    deadline: string
+}
+
+
 export const backendAPI = {
+    updateTodoList(todolistId: string, title: string) {
+        return instance.put<BaseResponseType>(`todo-lists/${todolistId}`, {title: title})
+    },
+    deleteTodoList(todolistId: string) {
+        return instance.delete<BaseResponseType>(`todo-lists/${todolistId}`)
+    },
+    createTodoList(name: string) {
+        return instance.post<BaseResponseType<{ item: TodoListOnServerType }>>('todo-lists', {title: name})
+    },
     getTodoLists() {
         return instance.get<Array<TodoListOnServerType>>('todo-lists')
     },
-    deleteTodoList(todoListID: string) {
-        return instance.delete(`todo-lists/${todoListID}`)
+    createTask(todolistID: string, name: string) {
+        return instance.post<BaseResponseType<{ item: TaskType }>>(`todo-lists/${todolistID}/tasks`, {title: name})
     },
-    createTodoList(){
-
+    getTasks(todolistID: string) {
+        return instance.get<GetTasksResponseType>(`todo-lists/${todolistID}/tasks`)
     },
-    updateTodoList(){
-
+    updateTask(todolistID: string, taskID: string, newName: string, description: string) {
+        const requestBody: UpdateTaskRequestBodyType = {
+            title: newName,
+            description: description,
+            completed: false,
+            status: 1,
+            priority: 2,
+            startDate: '2022-02-05T13:03:26.157',
+            deadline: '2022-02-10T13:03:26.157',
+        }
+        return instance.put<BaseResponseType<{ item: TaskType }>>(`todo-lists/${todolistID}/tasks/${taskID}`, requestBody)
     },
-    getTasks(){
+    deleteTask(todolistID: string, taskID: string) {
+        return instance.delete<BaseResponseType>(`/todo-lists/${todolistID}/tasks/${taskID}`)
 
-    },
-    deleteTask(){
-
-    },
-    createTask(){
-
-    },
-    updateTask(){
-
-    },
-
+    }
 }
---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,170 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import ReactJson from "react-json-view";
+import {backendAPI} from "../api/it-inc-api";
+
+export default { //for storybook
+    title: 'API'
+}
+
+
+
+export const RequestsMiniApp = () => {
+    const [todoLists, setTodoLists] = useState<any>(null)
+    const [tasks, setTasks] = useState<any>(null)
+    const [response, setResponse] = useState<any>(null)
+    const [chosenTodoListID, setChosenTodoListID] = useState<any>(null)
+    const [chosenTaskID, setChosenTaskID] = useState<any>(null)
+    const [listNameToCreate, setListNameToCreate] = useState<any>(null)
+    const [taskNameToCreate, setTaskNameToCreate] = useState<any>(null)
+    const [taskDescription, setTaskDescription] = useState<any>(null)
+
+
+    const getListsFromServer = () => {
+        backendAPI.getTodoLists()
+            .then(response => setTodoLists(response.data))
+
+    }
+
+    const getTasksFromServer = () => {
+        if (chosenTodoListID) {
+            backendAPI.getTasks(chosenTodoListID)
+                .then(response => setTasks(response.data))
+        }
+        else{
+            alert('choose a todolist')
+        }
+    }
+    const createNewList = () => {
+        if(listNameToCreate) {
+            backendAPI.createTodoList(listNameToCreate)
+                .then(response=>setResponse(response))
+                .then(getListsFromServer)
+        }
+        else{
+            alert('new list name field is empty')
+        }
+        setListNameToCreate('')
+    }
+
+    const deleteListFromServer = () => {
+        if (chosenTodoListID) {
+            backendAPI.deleteTodoList(chosenTodoListID)
+                .then(response => setResponse(response))
+                .then(getListsFromServer)
+        }
+        return
+    }
+
+    const deleteTaskFromList = () => {
+        if(chosenTodoListID && chosenTaskID){
+            backendAPI.deleteTask(chosenTodoListID, chosenTaskID)
+                .then(response=>setResponse(response))
+                .then(getTasksFromServer)
+        }
+        else{
+            alert('not enough data provided to make the request')
+        }
+
+    }
+
+
+    const createTaskInChosenList = () => {
+        if (chosenTodoListID && taskNameToCreate) {
+            backendAPI.createTask(chosenTodoListID, taskNameToCreate)
+                .then(response => setResponse(response))
+                .then(getTasksFromServer)
+        }
+        else{
+            alert('new task name field is empty')
+        }
+        setTaskNameToCreate('')
+
+    }
+
+    const changeTaskName = () => {
+        if(chosenTodoListID && chosenTaskID && taskNameToCreate && taskDescription){
+            backendAPI.updateTask(chosenTodoListID, chosenTaskID, taskNameToCreate, taskDescription)
+                .then(response=>setResponse(response))
+                .then(getTasksFromServer)
+        }
+        else{
+            alert('not enough info to proceed')
+        }
+
+    }
+
+    const changeListName = () => {
+        if (listNameToCreate && chosenTodoListID) {
+            backendAPI.updateTodoList(chosenTodoListID, listNameToCreate)
+                .then(response => setResponse(response))
+                .then(getListsFromServer)
+        } else {
+            alert('not enough info to proceed')
+        }
+    }
+
+
+    // todolistAPI.createTask(todolistId, taskName).then(response=>setState(response.data))
+
+
+    return (
+        <div>
+            <div>
+                <h3>todoLists</h3>
+                <ReactJson src={todoLists}/>
+            </div>
+            <div>
+                <h3>Tasks</h3>
+                <ReactJson src={tasks}/>
+            </div>
+            <div>
+                <h3>Response</h3>
+                <ReactJson src={response}/>
+            </div>
+            <h3>{`chosen list is ${chosenTodoListID}`}</h3>
+            <div>
+
+                <input type="text" placeholder={'todolistID'}
+                       onChange={(e) => setChosenTodoListID(e.currentTarget.value)}
+                       value={chosenTodoListID} style={{width:"250px"}}/>
+                <input type="text" placeholder={'taskID'}
+                       onChange={(e) => setChosenTaskID(e.currentTarget.value)}
+                       value={chosenTaskID} style={{width:"250px"}}/>
+            </div>
+            <div>
+                <input type="text" placeholder={'new list name'}
+                       onChange={(e) => setListNameToCreate(e.currentTarget.value)}
+                       value={listNameToCreate} style={{width:"250px"}}/>
+                <input type="text" placeholder={'new task name'}
+                       onChange={(e) => setTaskNameToCreate(e.currentTarget.value)}
+                       value={taskNameToCreate} style={{width:"250px"}}/>
+            </div>
+            <div >
+                <textarea placeholder={'task description'} value={taskDescription}
+                          onChange={(e)=> setTaskDescription(e.currentTarget.value)}
+                          style={{height:"80px", minWidth:"500px"}}/>
+            </div>
+
+            <div>
+                <div>
+                    <button onClick={getListsFromServer}>get todolists</button>
+                    <button onClick={getTasksFromServer}>get tasks</button>
+                    <button onClick={createTaskInChosenList}>create task</button>
+                    <button onClick={createNewList}>create new list</button>
+                </div>
+                <div>
+                    <button onClick={deleteListFromServer}>delete chosen list</button>
+                    <button onClick={changeTaskName}>change task</button>
+                    <button onClick={changeListName}>change list name</button>
+                    <button onClick={deleteTaskFromList}>delete task</button>
+                </div>
+
+
+            </div>
+        </div>
+
+
+    )
+}
+
