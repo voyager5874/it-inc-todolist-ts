6f93090513cfd2dc05e2e095fdeefa80e0df2084
Aleksandr Savkin <voyager5874@gmail.com>
2022-02-15 23:05:11 +0300

    Updates

--- precache-manifest.b93b48b82367919a746c0eb4792539a4.js
+++ precache-manifest.b93b48b82367919a746c0eb4792539a4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7d963ad7e9f2217e66602942803c7e4",
+    "revision": "c60567610b709c3714ce1df8f08efe39",
     "url": "/it-inc-todolist-ts/index.html"
   },
   {
-    "revision": "637c95c208f28625ac8b",
+    "revision": "d1eb2ba3ddcae0c101ee",
     "url": "/it-inc-todolist-ts/static/css/main.e5b21dbf.chunk.css"
   },
   {
-    "revision": "d951508089eb48de04c0",
-    "url": "/it-inc-todolist-ts/static/js/2.509d6dd0.chunk.js"
+    "revision": "4aff1b1901a8986e6e9d",
+    "url": "/it-inc-todolist-ts/static/js/2.226a8727.chunk.js"
   },
   {
     "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
-    "url": "/it-inc-todolist-ts/static/js/2.509d6dd0.chunk.js.LICENSE.txt"
+    "url": "/it-inc-todolist-ts/static/js/2.226a8727.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "637c95c208f28625ac8b",
-    "url": "/it-inc-todolist-ts/static/js/main.97e2f9d5.chunk.js"
+    "revision": "d1eb2ba3ddcae0c101ee",
+    "url": "/it-inc-todolist-ts/static/js/main.8bbdd2f1.chunk.js"
   },
   {
     "revision": "50abc6d0122b09419380",
